392966a480c149f233ebdf198b2f2fb8a62d1c17
Adam Booth <adam_booth@live.co.uk>
2026-01-29 17:13:14 +0000

    added post edit with creator and auth check

--- src/app/posts/editPost/[id]/page.js
+++ src/app/posts/editPost/[id]/page.js
@@ -1,15 +1,43 @@
 import { requireProfilePostMatch } from "@/utils/requireProfilePostMatch";
+import { db } from "@/utils/.dbConnection";
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 
 export default async function editPostPage({ params }) {
   await requireProfilePostMatch({ params });
-
   const { id } = await params;
-  console.log("Post ID in page:", id);
 
   return (
     <>
-      <h1>Edit Post Page</h1>
-      <p>Editing post {id}</p>
+      <div classcontent="edit-post-container">
+        <div classcontent="form-wrapper">
+          <h1>Edit Post</h1>
+
+          <form
+            action={async (formData) => {
+              "use server";
+
+              const updatedcontent = formData.get("content");
+
+              await db.query(
+                `UPDATE week9posts 
+               SET content = $1 WHERE id = $2`,
+                [updatedcontent, id],
+              );
+
+              revalidatePath(`/posts`);
+              redirect(`/posts`);
+            }}
+          >
+            <label>Content :</label>
+            <input type="text" name="content" required />
+
+            <button className="submit-button" type="submit">
+              Save
+            </button>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
